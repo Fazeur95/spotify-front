1cfc0b5a0ed51b2bbd4db180cc7ab173a884fd8f
Faouizi MZEBLA <faouizi.mzebla@ynov.com>
2024-01-04 19:00:11 +0100

    Add useRef hook to create a reference to the audio element

--- src/pages/TrackTogetherPage/index.jsx
+++ src/pages/TrackTogetherPage/index.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useEffect, useState, useContext, useRef} from 'react';
 import {useParams} from 'react-router-dom';
 import styled from 'styled-components';
 import {AudioPlayerContext} from '../../utils/context/AudioPlayerContext/AudioPlayerContext'; // Import the context
@@ -7,6 +7,7 @@ const TrackTogetherPage = () => {
   const {id} = useParams(); // Get the track ID from the URL
   const {socket} = useContext(AudioPlayerContext); // Access the socket
   const [track, setTrack] = useState(null);
+  const audioRef = useRef(); // Create a reference to the audio element
 
   useEffect(() => {
     // Fetch the track details from the server or a database
@@ -14,8 +15,13 @@ const TrackTogetherPage = () => {
       .then(response => response.json())
       .then(data => setTrack(data))
       .catch(error => console.error(error));
-    console.log(track);
   }, [id]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.load(); // Reload the audio source
+    }
+  }, [track]);
 
   useEffect(() => {
     if (socket == null) return;
@@ -52,10 +58,12 @@ const TrackTogetherPage = () => {
         <TrackArtist>{track.album}</TrackArtist>
         {/* Add more track details here... */}
       </TrackInfo>
-      <StyledAudio controls src={track.audioUrl} />
+      <StyledAudio ref={audioRef} controls src={track.url} />
     </TrackPageContainer>
   );
 };
+
+// Your styled components...
 
 const TrackPageContainer = styled.div`
   display: flex;
